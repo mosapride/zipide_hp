f62b5452ee3a40cf849dccdc2b0005f9de8edde7
mosapride <mosapride01@gmail.com>
2021-11-23 13:27:25 +0900

    fix

--- docs/.vuepress/components/CallInFeedAdsense.vue
+++ docs/.vuepress/components/CallInFeedAdsense.vue
@@ -4,7 +4,6 @@
     <InFeedAdsense
       data-ad-client="ca-pub-6855447887247821"
       data-ad-slot="2671429676"
-      data-ad-layout-key="xxxxxxxx"
      style="display:block;height:80px"
      data-ad-format="fluid"
      data-ad-layout="image-top"
